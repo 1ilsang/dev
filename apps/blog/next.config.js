4bc24995f17b62f0da890a16298c81860449131c
1ilsang <1ilsang@naver.com>
2022-06-20 00:39:02 +0900

    feat(image): Add character image

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -4,8 +4,7 @@ module.exports = {
     // outputFileTracingRoot: path.join(__dirname, "../../packages/content/posts"),
   },
   images: {
-    loader: "imgix",
-    path: "https://1ilsang.dev/",
-    domains: ["avatars.githubusercontent.com"],
+    loader: "akamai",
+    path: "",
   },
 };
