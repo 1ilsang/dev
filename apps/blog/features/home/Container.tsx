c83c75c92f69041821ca28922086829182c36434
1ilsang <1ilsang@naver.com>
2023-08-31 19:00:55 +0900

    feat(video): Add playsinline

--- apps/blog/features/home/Container.tsx
+++ apps/blog/features/home/Container.tsx
@@ -3,7 +3,14 @@ import { FunctionComponent } from "react";
 const HomeContainer: FunctionComponent = () => {
   return (
     <div className="home-container">
-      <video className="video" src="/assets/open.mp4" autoPlay loop muted />
+      <video
+        className="video"
+        src="/assets/open.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+      />
     </div>
   );
 };
