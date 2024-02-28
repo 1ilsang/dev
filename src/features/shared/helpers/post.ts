6ea9f4b1daf8d70c8e5abfbd20cc6d807bf7cf15
1ilsang <1ilsang@naver.com>
2024-02-29 00:18:27 +0900

    chore(posts): Enhancement types

--- src/features/shared/helpers/post.ts
+++ src/features/shared/helpers/post.ts
@@ -7,6 +7,8 @@ import { join } from 'path';
 
 import matter from 'gray-matter';
 
+import { PostType } from '~/posts/models';
+
 const rootPostPath = join(process.cwd(), '_posts');
 
 export const getPostSlugs = (
@@ -31,17 +33,16 @@ export const getPostSlugs = (
   return mergedFiles;
 };
 
-export const getPostBySlug = (slug: string, fields: string[] = []) => {
+export const getPostBySlug = (
+  slug: string,
+  fields: (keyof PostType)[] = [],
+) => {
   const realSlug = slug.replace(/\.md$/, '');
   const fullPath = join(rootPostPath, `${realSlug}.md`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
   const { data, content } = matter(fileContents);
 
-  type Items = {
-    [key: string]: string;
-  };
-
-  const items: Items = {};
+  const items = {} as PostType;
 
   // Ensure only the minimal needed data is exposed
   fields.forEach((field) => {
@@ -59,7 +60,7 @@ export const getPostBySlug = (slug: string, fields: string[] = []) => {
   return items;
 };
 
-export const getAllPosts = (fields: string[] = []) => {
+export const getAllPosts = (fields: (keyof PostType)[] = []) => {
   const slugs = getPostSlugs();
   const posts = slugs
     .map((slug) => getPostBySlug(slug, [...fields, 'date']))
