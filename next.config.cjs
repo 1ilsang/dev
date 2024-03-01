89b7bfedac85f74f90eae384b309823e324d9ae3
1ilsang <1ilsang@naver.com>
2024-03-02 00:23:05 +0900

    feat(next): Add config

--- next.config.cjs
+++ next.config.cjs
@@ -1,9 +1,8 @@
-/** @type {import('next').NextConfig} */
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   experimental: {
     // this includes files from the monorepo base two directories up
     // outputFileTracingRoot: path.join(__dirname, "../../packages/content/posts"),
@@ -16,3 +15,5 @@ module.exports = {
     includePaths: [path.join(__dirname, 'styles')],
   },
 };
+
+module.exports = nextConfig;
