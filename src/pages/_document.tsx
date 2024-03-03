fe9dd7106a7d02161101a66d441346608e058641
1ilsang <1ilsang@naver.com>
2024-03-03 23:45:22 +0900

    chore: Remove unused hightlight css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,10 +11,6 @@ export default class MyDocument extends Document {
           <meta name="author" content="1ilsang" />
           <meta name="keyword" content="1ilsang,dev,front,frontend,react" />
           <link
-            rel="stylesheet"
-            href="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.7.0/build/styles/github-dark.min.css"
-          />
-          <link
             href="https://cdn.jsdelivr.net/gh/everydayminder/assets/subset-D2Coding.woff2"
             as="font"
             type="font/woff2"
