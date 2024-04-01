9319a84c6f7e5bd4cf9756ec967a683f0fcefe79
1ilsang <1ilsang@naver.com>
2024-04-01 21:56:08 +0900

    chore: 프론트 to 프런트

--- _data/activity.ts
+++ _data/activity.ts
@@ -158,7 +158,7 @@ const openSource: Activity[] = [
 const SNS: Activity[] = [
   {
     type: ActivityType.sns,
-    name: '재택 근무하며 회사에서 친구 만들기(feat. 라인 프론트엔드 개발자)',
+    name: '재택 근무하며 회사에서 친구 만들기(feat. 라인 프런트엔드 개발자)',
     url: 'https://www.youtube.com/watch?v=zr3WPa_aIcU&ab_channel=%EB%9D%BC%EC%9D%B8%EA%B0%9C%EB%B0%9C%EC%8B%A4%EB%A1%9D',
     startDate: 1648047600000,
     endDate: 1648047600000,
