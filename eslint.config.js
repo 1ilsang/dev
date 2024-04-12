b7e88d40de929246454b3a8fe33e9247ec8cc849
1ilsang <1ilsang@naver.com>
2024-04-13 02:14:31 +0900

    feat(eslint): Add out ignore

--- eslint.config.js
+++ eslint.config.js
@@ -12,6 +12,6 @@ export default [
     // https://github.com/import-js/eslint-plugin-import/pull/2996
     // plugins: eslint-plugin-import
   },
-  { ignores: ['.next', '*.cjs'] },
+  { ignores: ['.next', 'out', '*.cjs'] },
   eslintConfigPrettier,
 ];
