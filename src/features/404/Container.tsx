f79cd053c0395e9c02172713ab274875e5d4463e
1ilsang <1ilsang@naver.com>
2024-05-13 00:16:58 +0900

    feat(404): Remove audio

--- src/features/404/Container.tsx
+++ src/features/404/Container.tsx
@@ -16,29 +16,13 @@ const NotFoundContainer: FunctionComponent = () => {
       <div>
         <img src="/assets/404.webp" alt="surfing" width={720} height={580} />
       </div>
-      <audio
-        className="mt-4"
-        src="/assets/jackpot.mp4"
-        muted
-        controls
-        autoPlay
-        loop
-      />
       <pre>
         <code>{description}</code>
         <div className="small">
-          {' '}
           Image Copyright:&nbsp;
           <ExternalLink
             label="Freepik"
             href="https://kr.freepik.com/free-vector/isometric-summer-beach-vacation-concept_9515534.htm"
-          />
-        </div>
-        <div className="small">
-          Audio:&nbsp;
-          <ExternalLink
-            label="TheFatRat - Jackpot"
-            href="https://youtu.be/kL8CyVqzmkc"
           />
         </div>
       </pre>
