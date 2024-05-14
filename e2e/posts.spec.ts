cb22e38d9fc63d1536209c2959a2e40c48a4d8eb
1ilsang <1ilsang@naver.com>
2024-05-14 21:31:06 +0900

    docs: Add typescript-subtyping

--- e2e/posts.spec.ts
+++ e2e/posts.spec.ts
@@ -1,11 +1,15 @@
 import { expect, test } from '@playwright/test';
 import { MACRO_SUITE } from './shared/constants';
-import { gotoUrl } from './shared/utils';
+import { gotoUrl, screenshotFullPage } from './shared/utils';
 
 test.describe('posts', () => {
   test(MACRO_SUITE.DOM_SNAPSHOT, async ({ page }) => {
     await gotoUrl({ page, url: '/posts' });
     const body = await page.locator('#__next').innerHTML();
     expect(body).toMatchSnapshot([`posts.html`]);
+  });
+
+  test(MACRO_SUITE.SCREEN_SNAPSHOT, async ({ page }) => {
+    await screenshotFullPage({ page, url: '/posts', arg: [`posts.png`] });
   });
 });
