2071e39ee271aff60911e607b386e101f2b209b5
1ilsang <1ilsang@naver.com>
2024-08-11 22:28:36 +0900

    test: Update test

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from 'next';
 import PostListContainer from '~/posts/Container';
 import { type PostType } from '~/posts/models';
 import Footer from '~/shared/components/Footer';
+import { MainLayout } from '~/shared/components/MainLayout';
 import Navbar from '~/shared/components/nav/Navbar';
 import { getAllPosts } from '~/shared/helpers/post';
 
@@ -19,11 +20,11 @@ const PostHome: NextPage = () => {
   ]);
 
   return (
-    <>
+    <MainLayout>
       <Navbar />
       <PostListContainer posts={posts} />
       <Footer />
-    </>
+    </MainLayout>
   );
 };
 
