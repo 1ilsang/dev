d0b4275e5c7f7e87de272dd939c0b3dff9b3a7ba
1ilsang <1ilsang@naver.com>
2024-12-07 20:46:48 +0900

    feat: Bump to react 18 to 19

--- src/features/shared/portal/Container.tsx
+++ src/features/shared/portal/Container.tsx
@@ -8,9 +8,6 @@ type PortalProps = PropsWithChildren & {
   id?: string;
 };
 
-// FIXME: React 19.0.0-rc.1 has a bug with the type of createPortal
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
 export const Portal: FunctionComponent<PortalProps> = ({
   children,
   id = 'portal',
@@ -23,7 +20,5 @@ export const Portal: FunctionComponent<PortalProps> = ({
     throw new Error(`Not found Portal id: ${id}`);
   }
 
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
   return createPortal(children, el);
 };
