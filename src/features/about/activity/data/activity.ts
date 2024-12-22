5e11dbc2c06fc7fb741d8ac70be200ac96545b4e
1ilsang <1ilsang@naver.com>
2024-12-23 02:46:59 +0900

    feat(about): OSS 수상 추가

--- src/features/about/activity/data/activity.ts
+++ src/features/about/activity/data/activity.ts
@@ -4,7 +4,14 @@ import { ActivityType } from '~/about/activity/models';
 const award: Activity[] = [
   {
     type: ActivityType.award,
-    name: '산업융합 아이디어 경진대회 장관상',
+    name: '오픈소스 컨트리뷰션 Node.js - 과학기술정보통신부 장관상',
+    url: 'https://www.oss.kr/notice/show/b6c4ed79-6435-444b-b4b0-debbb041354f',
+    startDate: 1731596400000,
+    endDate: 1731596400000,
+  },
+  {
+    type: ActivityType.award,
+    name: '산업융합 아이디어 경진대회 - 산업통상자원부 장관상',
     url: 'https://blog.naver.com/1ilsang/221169478287',
     startDate: 1505660400000,
     endDate: 1513868400000,
