f679222a785f84a3b4f1a9f94d4405ccb1b2c4c6
1ilsang <1ilsang@naver.com>
2025-01-06 01:48:16 +0900

    chore(about): 우아콘 추가

--- src/features/about/activity/data/seminar.ts
+++ src/features/about/activity/data/seminar.ts
@@ -3,6 +3,12 @@ import { ActivityType } from '~/about/activity/models';
 
 export const seminar: Activity[] = [
   {
+    name: '우아콘 이그나이트 - 오픈소스 참여 경험',
+    url: '/posts/2024-woowa-ignite',
+    startDate: 1730214000000,
+    endDate: 1730214000000,
+  },
+  {
     name: '모여봐요 오픈소스의 숲',
     url: '/posts/geultto8-open-source-seminar',
     startDate: 1693567800000,
