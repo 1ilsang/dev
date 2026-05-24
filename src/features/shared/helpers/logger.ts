afdbc90b0fe7580c773324cc223d5ec8daa0065b
1ilsang <1ilsang.dev@gmail.com>
2026-05-25 07:13:06 +0900

    feat(e2e): actions 개선

--- src/features/shared/helpers/logger.ts
+++ src/features/shared/helpers/logger.ts
@@ -1,7 +1,7 @@
 import { sendGAEvent } from '@next/third-parties/google';
 
 export const infoLog = (message: string) => {
-  console.info(`\n\x1b[36m ✓ ${message} \x1b[0m`);
+  console.info(`\x1b[36m ✓ ${message} \x1b[0m`);
 };
 
 export type GaActionType =
